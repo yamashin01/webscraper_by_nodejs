eea2cec42626549c62d02e607ce55555d8fd2ee8
yamada <shinya.yamada@benext-corp.com>
2022-10-23 13:41:04 +0900

    ♻️ refactoring index.js

--- index.js
+++ index.js
@@ -4,24 +4,22 @@ const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const  app = express();
+const app = express();
 
-// ウェブスクレイバーを作ろう
+// create webscraper
 const URL = "https://search.rakuten.co.jp/search/mall/keyboard/";
-const data = [];
 
 axios(URL).then((response) => {
     const htmlParser = response.data;
-    // console.log(htmlParser);
-
+    
     const $ = cheerio.load(htmlParser);
-
-    $(".searchresultitem", htmlParser).each(function() {
+    
+    const dataList = $(".searchresultitem", htmlParser).map(function() {
         const title = $(this).find(".title").text();
         const price = $(this).find(".important").text();
-        data.push({title, price});
-        console.log(data);
-    })
+       return {title, price};
+    });
+    console.log(dataList);
 }).catch(error => console.log(error));
 
 app.listen(PORT, console.log("server running!"));
